10e870c414d2a6f1f94a67f98f45638d05fe6c4a
iarthofer16 <isabelle.arthofer@gmail.com>
2019-03-28 10:06:40 +0100

    MainActivity layout

--- app/src/main/java/net/htlgrieskirchen/pos3/iarthofer16woche24/MainActivity.java
+++ app/src/main/java/net/htlgrieskirchen/pos3/iarthofer16woche24/MainActivity.java
@@ -3,11 +3,17 @@ package net.htlgrieskirchen.pos3.iarthofer16woche24;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.firebase.firestore.FirebaseFirestore;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+
     }
 }
